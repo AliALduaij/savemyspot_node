99ae3c33a239f9d2078f259d047c30b8130d1b89
AliAlduaij <aliaalduaij@gmail.com>
2020-03-31 00:41:56 +0300

    wierd

---
+++ index.js
@@ -0,0 +1,64 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var axios = require("axios");
+
+//this is the url of the backend
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/"
+});
+
+io.on("connection", function(socket) {
+  console.log("connected", socket.id);
+});
+
+http.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
+
+function getRestaurantQ(socket, restaurantID, user) {
+  // gitting the information of a restaurant
+  instance
+    .get(`queue/list/`, { data: { restaurant: restaurantID } })
+    .then(res => res.data)
+    .then(queue => {
+      socket.join(queue.id); // typo
+      let found = false;
+      if (queue.length > 0) {
+        queue.forEach(spot => {
+          if (user !== null && spot.user.id === user) {
+            io.to(socket.id).emit("user spot", {
+              spot: spot
+            });
+            found = true;
+          }
+        });
+        if (!found) {
+          io.to(socket.id).emit("user spot", {
+            spot: null
+          });
+        }
+        io.to(socket.id).emit("q info", {
+          restaurantQ: queue[0].position
+        });
+      } else {
+        io.to(socket.id).emit("q info", {
+          restaurantQ: 0
+        });
+        io.to(socket.id).emit("user spot", {
+          spot: nul
+        });
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+io.on("connection", function(socket) {
+  socket.on("restaurant room", function(data) {
+    socket.join(data.restaurant.id);
+    getRestaurantQ(socket, data.restaurant.id, data.user);
+  });
+  socket.on("back", function(data) {
+    socket.leave(data);
+  });
+});
